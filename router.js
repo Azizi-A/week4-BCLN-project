aeddb3be530c662a1d83faaaee52942112bfc9c4
Azizi Adeyemo <azizi.adeyemo@gmail.com>
2020-08-05 16:10:10 +0100

    Router calls relevant functions
    Relates #7
    
    Co-authored-by: Jennifer B <jennifer.sesaybarnes@gmail.com>

--- router.js
+++ router.js
@@ -1,10 +1,22 @@
-//check url and req method
+const home = require(path.join(__dirname, "handlers", "home"));
+const submit = require(path.join(__dirname, "handlers", "submit"));
+const missing = require(path.join(__dirname, "handlers", "missing"));
 
-//if url is home and method is get,
-//serve homepage
+const router = (request, response) => {
+  //check url and req method
+  const url = request.url;
+  const method = request.method;
 
-//if url is submit and method is post,
-//receive the data packets and build data object
-//process data
-//tbc...
-//if there is no url route to 404
+  //if url is home and method is get,
+  if (url === "/") {
+    //serve homepage
+    home(request, response);
+    //if url is submit and method is post,
+  } else if (url === "/submit" && method === "POST") {
+    submit(request, response);
+  } else {
+    missing(request, response);
+  }
+};
+
+module.exports = router;
