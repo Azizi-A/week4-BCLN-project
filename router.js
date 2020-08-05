af8b2c99944f39e48f9b83344c07cefad13d4009
Azizi Adeyemo <azizi.adeyemo@gmail.com>
2020-08-05 15:52:24 +0100

    Set up server and implemented prettier style file
    Relates #7

---
+++ router.js
@@ -0,0 +1,10 @@
+//check url and req method
+
+//if url is home and method is get,
+//serve homepage
+
+//if url is submit and method is post,
+//receive the data packets and build data object
+//process data
+//tbc...
+//if there is no url route to 404
