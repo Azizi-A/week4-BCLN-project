b996b8a697dc12db7ab3ce0236f1ff0ff91c56b0
jenndroid <jennifer.sesaybarnes@gmail.com>
2020-08-06 11:22:14 +0100

    Basic submit handler to test Relates #9 Co-authored-by: Jihyun-Jang <jhjang742@gmail.com>

--- handlers/submit.js
+++ handlers/submit.js
@@ -1,3 +1,24 @@
 //receive the data packets and build data object
 //process data
 //tbc...
+function submit(request, response) {
+    let body = "";
+    // callback runs every time the stream has the next bit of data
+    request.on("data", chunk => {
+      body += chunk;
+    });
+    // callback runs when request finishes and we have all the data
+    request.on("end", () => {
+      console.log(body); // we should have the whole request body now
+        //create a template 
+
+        //place thought value in thought tag
+
+        //place author
+      response.writeHead(200, { "content-type": "text/html" });
+      response.end(
+          //send back index.html
+
+        );
+    });
+  }
