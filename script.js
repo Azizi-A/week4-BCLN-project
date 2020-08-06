cb7ff244c48f80bc0784ab6fb3d77ec04b51f7c1
Azizi Adeyemo <azizi.adeyemo@gmail.com>
2020-08-06 17:30:07 +0100

    Fetch and show posts
    Co-authored-by: Jihyun-Jang <jhjang742@gmail.com>
    Co-authored-by: Jennifer B <jennifer.sesaybarnes@gmail.com>
    Co-authored-by: amberrignell <a.rignell16@ejm.org>

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
 const submitBtn = document.querySelector(".form__button-submit");
 const inputThought = document.querySelector(".form__input-thought");
 const inputAuthor = document.querySelector(".form__input-author");
+const postsList = document.querySelector(".main__article-posts");
+const template = document.querySelector(".template");
+
+fetch("/get-post")
+  .then(res => {
+    if (!res.ok) {
+      throw new Error("Server error");
+      return res;
+    }
+  })
+  .then(res => {
+    res.json();
+  })
+  .then(data => {
+    postsList.innerHTML = "";
+    for (i = 0; i < data.length; i++) {
+      //loop through each object in the array (which we're getting as our response)
+      // create post
+      const post = data[i]; // select the relevant object from the array (should be something like {thought: 'Cows are cute', author: 'Bob'})
+      const newStory = template.content.cloneNode(true); //get the read-only document fragment from the template and clone it
+      let thought = newStory.querySelector(".template__article-thought"); //get the thought element in the fragment thats just been created
+      let author = newStory.querySelector(".template__article-author"); //get the thought element in the fragment thats just been created
+      thought.textContent = post.thought;
+      author.textContent = post.author;
+
+      let deleteButton = newStory.querySelector(".template__button-delete");
+      // deleteButton.addEventListener("click", () => {
+      //   fetch("/", { method: "DELETE", body: key }).then(location.reload());
+      // });
+
+      // adds story into index.html
+      postsList.appendChild(newStory);
+    }
+  })
+  .catch(err => console.error(err));
 
 submitBtn.addEventListener("submit", event => {
   event.preventDefault();
